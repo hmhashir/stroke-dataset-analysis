9b268e7ba823037cdf26d02ad13166c09163fbfa
Muhammad Hashir <contact.hmhashir@gmail.com>
2024-12-27 17:42:24 +0500

    print statement added

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,3 +3,6 @@ import pandas as pd
 #Load the DataSet:
 data = pd.read_csv('F:\Data Science\Intro to Data Science\stroke-dataset-analysis\stroke-data.csv')
 print(data.head())
+
+if __name__ == "__main__":
+    print("Data loaded successfully.")
