b4f6e0447d85d7ac1f5199f2596d307cf6d20443
Muhammad Hashir <contact.hmhashir@gmail.com>
2024-12-27 18:03:48 +0500

    summary added

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import pandas as pd
 #Load the DataSet:
 data = pd.read_csv('F:\Data Science\Intro to Data Science\stroke-dataset-analysis\stroke-data.csv')
 print(data.head())
+
+#SummaRy Statistics:
+print('Summary Statistics:\n', data.describe())
