3a29812dc041ad610a22f8403a1d83fcd21eb597
Muhammad Hashir <contact.hmhashir@gmail.com>
2024-12-27 18:14:14 +0500

    vusialization added

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import pandas as pd
 #Load the DataSet:
 data = pd.read_csv('F:\Data Science\Intro to Data Science\stroke-dataset-analysis\stroke-data.csv')
 print(data.head())
+print()
 
-#SummaRy Statistics:
+'''SummaRy Statistics:'''
 print('Summary Statistics:\n', data.describe())
+
+'''Visualizations:'''
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# sns.set(style = 'Whitegrid')
+
+#count plot for stroke:
+plt.figure(figsize=(8, 6))
+sns.countplot(x='stroke', data=data)
+plt.title('Count of Strokes')
+plt.xlabel('Stroke (0 = No, 1 = Yes)')
+plt.ylabel('Count')
+plt.show()
